2719fe63a173fcf9b16cf356559aef0eadcaac20
quantman <qiuzihua@qq.com>
2025-08-06 22:23:11 +0800

    QZH

--- Data/GetRich_IndexData_AK.py
+++ Data/GetRich_IndexData_AK.py
@@ -135,16 +135,89 @@ def get_index_zz_valuation(symbol, start_date, end_date):
     df = ak.stock_zh_index_hist_csindex(symbol=symbol, start_date=start_date, end_date=end_date)
     return df
 
+# 14. 申万指数实时行情
+def get_index_sw_realtime(symbol):
+    """
+    申万指数实时行情
+    symbol	str	symbol="基础一级"; choice of {"基础一级", "基础二级", "基础三级", "特色指数"}
+    """
+    df = ak.index_realtime_fund_sw()
+    return df
+
+# 15. 申万基金指数历史行情
+def get_index_sw_history(symbol, period):
+    """
+    申万指数历史行情
+    symbol	str	symbol="807200"; 基金指数代码
+    period	str	period="day"; choice of {"day", "week", "month"}
+    """
+    df = ak.index_hist_fund_sw(symbol=symbol, period=period)
+    return df
+
+# 16. 申万指数实时行情-sw
+def get_index_sw_realtime_sw(symbol):
+    """
+    申万指数实时行情-sw
+    symbol	str	symbol="市场表征"; choice of {"市场表征", "一级行业", "二级行业", "风格指数", "大类风格指数", "金创指数"}
+    """
+    df = ak.index_realtime_sw(symbol=symbol)
+    return df
+
+# 17. 申万指数历史行情-sw
+def get_index_sw_history_sw(symbol, period):
+    """
+    申万指数历史行情-sw
+    symbol    str    symbol="807200"; 基金指数代码
+    period    str    period="day"; choice of {"day", "week", "month"}
+    """
+    df = ak.index_hist_sw(symbol=symbol, period=period)
+    return df
+
+
+# 18. 申万指数分时行情
+def get_index_sw_minute(symbol):
+    """
+    申万指数分时行情
+    symbol	str	symbol="801030"; 指数代码
+    """
+    df = ak.index_min_sw(symbol=symbol)
+    return df
+
+# 19. 申万指数成分股
+def get_index_sw_stock(symbol):
+    """
+    申万指数成分股
+    symbol    str    symbol="801030"; 指数代码
+    """
+    df = ak.index_component_sw(symbol=symbol)
+    return df
+
+
+# 20. 申万指数-日报表
+def get_index_sw_daily_report(symbol, start_date, end_date):
+    """
+    申万指数-日报表
+    symbol	str	symbol="市场表征"; choice of {"市场表征", "一级行业", "二级行业", "风格指数"}
+    start_date	str	start_date="20221103"
+    end_date	str	end_date="20221103"
+    """
+    df = ak.index_analysis_daily_sw(symbol=symbol, start_date=start_date, end_date=end_date)
+    return df
 
 
 
 
 
 
-if __name__ == "__main__":
+
+
+
+
+# if __name__ == "__main__":
     # print(get_index_info_em())
     # print(get_index_stock_em("000300"))
-    print(get_index_zz_valuation("000300", "20180526", "20250805"))
+    # print(get_index_zz_valuation("000300", "20180526", "20250805"))
+
 
 
 
---
+++ Data/GetRich_MacroData_AK.py
@@ -0,0 +1,112 @@
+"""
+# -*- coding: utf-8 -*-
+从AKShare获取宏观数据
+Author: QiuZihua
+Date: 2025/08/01
+"""
+import akshare as ak
+import pandas as pd
+
+# 1. 获取中国宏观杠杆率
+def get_macro_leverage():
+    """
+    获取中国宏观杠杆率
+    :return:
+    """
+    df = ak.macro_cnbs()
+    return df
+
+# 2. 获取企业商品价格指数
+def get_macro_price_index():
+    """
+    获取企业商品价格指数
+    :return:
+    """
+    df = ak.macro_china_qyspjg()
+    return df
+
+# 3. 获取LPR利率
+def get_macro_lpr():
+    """
+    获取LPR利率
+    :return:
+    """
+    df = ak.macro_china_lpr()
+    return df
+
+# 4. 获取调查城镇失业率
+def get_macro_unemployment():
+    """
+    获取调查城镇失业率
+    :return:
+    """
+    df = ak.macro_china_urban_unemployment()
+    return df
+
+# 5. 获取社会融资规模增量统计
+def get_macro_shrz():
+    """
+    获取社会融资规模增量统计
+    :return:
+    """
+    df = ak.macro_china_shrzgm()
+    return df
+
+# 6. 获取月度CPI报告
+def get_macro_cpi_monthly():
+    """
+    获取月度CPI报告
+    :return:
+    """
+    df = ak.macro_china_cpi_monthly()
+    return df
+
+# 7. 能源指数
+def get_macro_energy():
+    """
+    获取能源指数
+    :return:
+    """
+    df = ak.macro_china_energy_index()
+    return df
+
+
+# 8. 获取大宗商品价格
+def get_macro_commodity():
+    """
+    获取大宗商品价格
+    :return:
+    """
+    df = ak.macro_china_commodity_price_index()
+    return df
+
+# 9. 新增信贷数据
+def get_macro_credit():
+    """
+    获取新增信贷数据
+    :return:
+    """
+    df = ak.macro_china_new_financial_credit()
+    return df
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # print(get_macro_leverage())
+    # print(get_macro_price_index())
+    # print(get_macro_lpr())
+    # print(get_macro_unemployment())
+    # print(get_macro_cpi_monthly())
+    # print(get_macro_shrz())
+    # print(get_macro_energy())
+    # print(get_macro_commodity())
+    print(get_macro_credit())
